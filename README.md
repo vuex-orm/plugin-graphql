782c0c2e665ceb76f200f3e5609204b7b247bb73
phortx <bk@itws.de>
2018-02-06 11:30:36 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # vuex-orm-apollo
+
 Vuex-ORM Plugin to sync the data against a GraphQL API via Apollo
+
+## Contribution
+
+```bash
+$ npm run build
+```
+
+Compile files and generate bundles in dist directory.
+
+```bash
+$ npm run lint
+```
+
+Lint files using a rule of Standard JS.
+
+```bash
+$ npm run test
+```
+
+Run the test using Mocha Webpack.
+
+```bash
+$ npm run coverage
+```
+
+Generate test coverage in coverage directory.
+
+
+## License
+
+Vuex ORM Apollo is open-sourced software licensed under the [MIT license](https://github.com/phortx/vuex-orm-apollo/blob/master/LICENSE.md).
---
+++ test/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const isCoverage = process.env.NODE_ENV === 'coverage';
+
+const rootDir = path.join(__dirname, '..');
+
+module.exports = {
+  target: 'node',
+
+  devtool: 'inline-cheap-module-source-map',
+
+  externals: [nodeExternals({
+    whitelist: [/lodash-es/]
+  })],
+
+  module: {
+    loaders: [].concat(
+      isCoverage ? {
+        test: /\.(js|ts)/,
+        include: `${rootDir}/src`,
+        loader: 'istanbul-instrumenter-loader',
+        query: {
+          esModules: true
+        }
+      } : [],
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          `${rootDir}/src`,
+          `${rootDir}/test`,
+        ],
+        options: {
+          cacheDirectory: true
+        }
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        include: `${rootDir}/src`,
+        options: {
+          transpileOnly: true
+        }
+      }
+    )
+  },
+
+  resolve: {
+    alias: {
+      app: `${rootDir}/src`,
+      test: `${rootDir}/test`
+    },
+    extensions: ['.js', '.ts']
+  },
+
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      tsconfig: `${rootDir}/tsconfig.json`,
+      watch: [`${rootDir}/src`]
+    })
+  ]
+};
