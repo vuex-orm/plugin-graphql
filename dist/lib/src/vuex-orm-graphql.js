27603f4ec38f593ba813de96a8b9c1fd641f166c
phortx <bk@itws.de>
2018-10-24 10:37:45 +0200

    Refactoring

---
+++ dist/lib/src/vuex-orm-graphql.js
@@ -0,0 +1,10 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+var DummyClass = /** @class */ (function () {
+    function DummyClass() {
+    }
+    return DummyClass;
+}());
+export default DummyClass;
+//# sourceMappingURL=vuex-orm-graphql.js.map
---
+++ dist/lib/test/vuex-orm-graphql.test.js
@@ -0,0 +1,13 @@
+import DummyClass from "../src/vuex-orm-graphql";
+/**
+ * Dummy test
+ */
+describe("Dummy test", function () {
+    it("works if true is truthy", function () {
+        expect(true).toBeTruthy();
+    });
+    it("DummyClass is instantiable", function () {
+        expect(new DummyClass()).toBeInstanceOf(DummyClass);
+    });
+});
+//# sourceMappingURL=vuex-orm-graphql.test.js.map
