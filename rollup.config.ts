cf78a576f1e38253ab83b0d13d8e04389df85d40
phortx <bk@itws.de>
2018-11-15 15:06:52 +0100

    Bugfixes

--- rollup.config.ts
+++ rollup.config.ts
@@ -25,7 +25,7 @@ const pkg = require('./package.json');
 const libraryName = 'vuex-orm-graphql';
 
 export default {
-  input: `src/${libraryName}.ts`,
+  input: `src/index.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true }
@@ -49,6 +49,7 @@ export default {
 
     babel({
       exclude: 'node_modules/**',
+      plugins: ['external-helpers']
     }),
 
     uglify(),
