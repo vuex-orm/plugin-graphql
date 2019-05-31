a292b7dc2c404c7d65a9ccb782129b0aceba4b16
Todd Baur <todd@toadkicker.com>
2019-05-31 05:10:04 -0700

    added apolloClient option for passing a preconfigured client (#93)
    * added apolloClient option for passing a preconfigured Apollo client to the plugin.
    
    * dont setup link if apolloClient is used
    
    * setup client via function
    
    * added some guidance on configuring for AppSync
    
    * context hug the apolloClient option
    
    * added test for setting apolloClient
    
    * rebuild project:
    #	dist/vuex-orm-graphql.es5.js
    #	dist/vuex-orm-graphql.es5.js.map
    #	dist/vuex-orm-graphql.umd.js
    #	dist/vuex-orm-graphql.umd.js.map

--- test/integration/actions/fetch.spec.ts
+++ test/integration/actions/fetch.spec.ts
@@ -124,42 +124,42 @@ query User($id: ID!) {
   });
 
   describe("without ID but with filter with array", () => {
-      test("sends the correct query to the API", async () => {
+    test("sends the correct query to the API", async () => {
+      // @ts-ignore
+      await User.fetch(1);
+
+      const insertedData = await Post.insert({
+        data: {
+          title: "It works!",
+          content: "This is a test!",
+          published: false,
+          otherId: 15,
+          author: User.find(1)
+        }
+      });
+
+      const post = insertedData.posts[0];
+
+      const request = await recordGraphQLRequest(async () => {
         // @ts-ignore
-        await User.fetch(1);
+        await User.fetch({ profileId: 2, posts: [post] });
+      });
 
-        const insertedData = await Post.insert({
-          data: {
-            title: "It works!",
+      expect(request!.variables).toEqual({
+        profileId: 2,
+        posts: [
+          {
+            id: 1,
+            authorId: 1,
             content: "This is a test!",
-            published: false,
             otherId: 15,
-            author: User.find(1)
+            published: false,
+            title: "It works!"
           }
-        });
-
-        const post = insertedData.posts[0];
-
-        const request = await recordGraphQLRequest(async () => {
-          // @ts-ignore
-          await User.fetch({ profileId: 2, posts: [post] });
-        });
-
-        expect(request!.variables).toEqual({
-          profileId: 2,
-          posts: [
-            {
-              id: 1,
-              authorId: 1,
-              content: "This is a test!",
-              otherId: 15,
-              published: false,
-              title: "It works!"
-            }
-          ]
-        });
-        expect(request!.query).toEqual(
-          `
+        ]
+      });
+      expect(request!.query).toEqual(
+        `
 query Users($profileId: ID!, $posts: [PostFilter]!) {
   users(filter: {profileId: $profileId, posts: $posts}) {
     nodes {
@@ -175,9 +175,9 @@ query Users($profileId: ID!, $posts: [PostFilter]!) {
   }
 }
           `.trim() + "\n"
-        );
-      });
+      );
     });
+  });
 
   describe("without ID but with filter with object", () => {
     test("sends the correct query to the API", async () => {
