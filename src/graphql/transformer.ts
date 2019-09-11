9014297f519f3ffdc9c4674e23acb757c8d081ad
Colin Wirz <cwirz@pyango.ch>
2019-09-11 21:34:42 +0200

    Continue ORM

--- src/graphql/transformer.ts
+++ src/graphql/transformer.ts
@@ -1,15 +1,9 @@
-import { Data, Field } from "../support/interfaces";
-import Model from "../orm/model";
-import { Model as ORMModel, Relation } from "@vuex-orm/core";
-import Context from "../common/context";
-import {
-  clone,
-  downcaseFirstLetter,
-  isPlainObject,
-  pluralize,
-  singularize
-} from "../support/utils";
-import { ConnectionMode } from "../adapters/adapter";
+import { Data, Field } from '../support/interfaces'
+import Model from '../orm/model'
+import { Model as ORMModel, Relation } from '@vuex-orm/core'
+import Context from '../common/context'
+import { clone, downcaseFirstLetter, isPlainObject, pluralize, singularize } from '../support/utils'
+import { ConnectionMode } from '../adapters/adapter'
 
 /**
  * This class provides methods to transform incoming data from GraphQL in to a format Vuex-ORM understands and
@@ -36,22 +30,22 @@ export default class Transformer {
     outgoingRecords?: Map<string, Array<string>>,
     recursiveCall?: boolean
   ): Data {
-    const context = Context.getInstance();
-    const relations: Map<string, Relation> = model.getRelations();
-    const returnValue: Data = {} as Data;
-    if (outgoingRecords === undefined) outgoingRecords = new Map<string, Array<string>>();
-    if (recursiveCall === undefined) recursiveCall = false;
+    const context = Context.getInstance()
+    const relations: Map<string, Relation> = model.getRelations()
+    const returnValue: Data = {} as Data
+    if (outgoingRecords === undefined) outgoingRecords = new Map<string, Array<string>>()
+    if (recursiveCall === undefined) recursiveCall = false
 
-    Object.keys(data).forEach(key => {
-      const value = data[key];
+    Object.keys(data).forEach((key) => {
+      const value = data[key]
 
-      const isRelation = model.getRelations().has(key);
-      let isRecursion = false;
+      const isRelation = model.getRelations().has(key)
+      let isRecursion = false
 
       if (value instanceof Array) {
-        isRecursion = isRelation && this.isRecursion(outgoingRecords!, value[0]);
+        isRecursion = isRelation && this.isRecursion(outgoingRecords!, value[0])
       } else {
-        isRecursion = isRelation && this.isRecursion(outgoingRecords!, value);
+        isRecursion = isRelation && this.isRecursion(outgoingRecords!, value)
       }
 
       // shouldIncludeOutgoingField and the read param is tricky. In the initial call of this method
@@ -59,60 +53,40 @@ export default class Transformer {
       // it should be true when we transform the outgoing data for fetch (and false for the others)
       if (
         !isRecursion &&
-        this.shouldIncludeOutgoingField(
-          (recursiveCall as boolean) && read,
-          key,
-          value,
-          model,
-          whitelist
-        )
+        this.shouldIncludeOutgoingField((recursiveCall as boolean) && read, key, value, model, whitelist)
       ) {
-        let relatedModel = Model.getRelatedModel(relations.get(key)!);
+        let relatedModel = Model.getRelatedModel(relations.get(key)!)
 
         if (value instanceof Array) {
           // Either this is a hasMany field or a .attr() field which contains an array.
-          const arrayModel = context.getModel(singularize(key), true);
+          const arrayModel = context.getModel(singularize(key), true)
 
           if (arrayModel) {
-            this.addRecordForRecursionDetection(outgoingRecords!, value[0]);
-            returnValue[key] = value.map(v => {
-              return this.transformOutgoingData(
-                arrayModel || model,
-                v,
-                read,
-                undefined,
-                outgoingRecords,
-                true
-              );
-            });
+            this.addRecordForRecursionDetection(outgoingRecords!, value[0])
+            returnValue[key] = value.map((v) => {
+              return this.transformOutgoingData(arrayModel || model, v, read, undefined, outgoingRecords, true)
+            })
           } else {
             // Simple field, not a relation
-            returnValue[key] = value;
+            returnValue[key] = value
           }
-        } else if (typeof value === "object" && value.$id !== undefined) {
+        } else if (typeof value === 'object' && value.$id !== undefined) {
           if (!relatedModel) {
-            relatedModel = context.getModel((value as ORMModel).$self().entity);
+            relatedModel = context.getModel((value as ORMModel).$self().entity)
           }
 
-          this.addRecordForRecursionDetection(outgoingRecords!, value);
+          this.addRecordForRecursionDetection(outgoingRecords!, value)
 
           // Value is a record, transform that too
-          returnValue[key] = this.transformOutgoingData(
-            relatedModel,
-            value,
-            read,
-            undefined,
-            outgoingRecords,
-            true
-          );
+          returnValue[key] = this.transformOutgoingData(relatedModel, value, read, undefined, outgoingRecords, true)
         } else {
           // In any other case just let the value be what ever it is
-          returnValue[key] = value;
+          returnValue[key] = value
         }
       }
-    });
+    })
 
-    return returnValue;
+    return returnValue
   }
 
   /**
@@ -130,68 +104,58 @@ export default class Transformer {
     mutation: boolean = false,
     recursiveCall: boolean = false
   ): Data {
-    let result: Data | Array<Data> = {} as Data;
-    const context = Context.getInstance();
+    let result: Data | Array<Data> = {} as Data
+    const context = Context.getInstance()
 
     if (!recursiveCall) {
-      context.logger.group("Transforming incoming data");
-      context.logger.log("Raw data:", data);
+      context.logger.group('Transforming incoming data')
+      context.logger.log('Raw data:', data)
     }
 
     if (Array.isArray(data)) {
-      result = data.map((d: any) => this.transformIncomingData(d, model, mutation, true));
+      result = data.map((d: any) => this.transformIncomingData(d, model, mutation, true))
     } else {
-      Object.keys(data).forEach(key => {
+      Object.keys(data).forEach((key) => {
         if (data[key] !== undefined && data[key] !== null && key in data) {
           if (isPlainObject(data[key])) {
-            const localModel: Model = context.getModel(key, true) || model;
+            const localModel: Model = context.getModel(key, true) || model
 
             if (data[key].nodes && context.connectionMode === ConnectionMode.NODES) {
-              result[pluralize(key)] = this.transformIncomingData(
-                data[key].nodes,
-                localModel,
-                mutation,
-                true
-              );
+              result[pluralize(key)] = this.transformIncomingData(data[key].nodes, localModel, mutation, true)
             } else if (data[key].edges && context.connectionMode === ConnectionMode.EDGES) {
-              result[pluralize(key)] = this.transformIncomingData(
-                data[key].edges,
-                localModel,
-                mutation,
-                true
-              );
-            } else if (data["node"] && context.connectionMode === ConnectionMode.EDGES) {
-              result = this.transformIncomingData(data["node"], localModel, mutation, true);
+              result[pluralize(key)] = this.transformIncomingData(data[key].edges, localModel, mutation, true)
+            } else if (data['node'] && context.connectionMode === ConnectionMode.EDGES) {
+              result = this.transformIncomingData(data['node'], localModel, mutation, true)
             } else {
-              let newKey = key;
+              let newKey = key
 
               if (mutation && !recursiveCall) {
-                newKey = data[key].nodes ? localModel.pluralName : localModel.singularName;
-                newKey = downcaseFirstLetter(newKey);
+                newKey = data[key].nodes ? localModel.pluralName : localModel.singularName
+                newKey = downcaseFirstLetter(newKey)
               }
 
-              result[newKey] = this.transformIncomingData(data[key], localModel, mutation, true);
+              result[newKey] = this.transformIncomingData(data[key], localModel, mutation, true)
             }
           } else if (Model.isFieldNumber(model.fields.get(key))) {
-            result[key] = parseFloat(data[key]);
-          } else if (key.endsWith("Type") && model.isTypeFieldOfPolymorphicRelation(key)) {
-            result[key] = pluralize(downcaseFirstLetter(data[key]));
+            result[key] = parseFloat(data[key])
+          } else if (key.endsWith('Type') && model.isTypeFieldOfPolymorphicRelation(key)) {
+            result[key] = pluralize(downcaseFirstLetter(data[key]))
           } else {
-            result[key] = data[key];
+            result[key] = data[key]
           }
         }
-      });
+      })
     }
 
     if (!recursiveCall) {
-      context.logger.log("Transformed data:", result);
-      context.logger.groupEnd();
+      context.logger.log('Transformed data:', result)
+      context.logger.groupEnd()
     } else {
-      result["$isPersisted"] = true;
+      result['$isPersisted'] = true
     }
 
     // Make sure this is really a plain JS object. We had some issues in testing here.
-    return clone(result);
+    return clone(result)
   }
 
   /**
@@ -211,29 +175,32 @@ export default class Transformer {
     whitelist?: Array<String>
   ): boolean {
     // Always add fields on the whitelist.
-    if (whitelist && whitelist.includes(fieldName)) return true;
+    if (whitelist && whitelist.includes(fieldName)) return true
 
     // Ignore internal fields
-    if (fieldName.startsWith("$")) return false;
+    if (fieldName.startsWith('$')) return false
+
+    // Ignore skiped fields
+    if (model.baseModel.skipFields && model.baseModel.skipFields.indexOf(fieldName) >= 0) return false
 
     // Ignore empty fields
-    if (value === null || value === undefined) return false;
+    if (value === null || value === undefined) return false
 
     // Include all eager save connections
     if (model.getRelations().has(fieldName)) {
       // We never add relations to filters.
-      if (forFilter) return false;
+      if (forFilter) return false
 
-      const relation: Relation = model.getRelations().get(fieldName)!;
-      const related: Model | null = Model.getRelatedModel(relation);
-      if (related && model.shouldEagerSaveRelation(fieldName, relation, related)) return true;
+      const relation: Relation = model.getRelations().get(fieldName)!
+      const related: Model | null = Model.getRelatedModel(relation)
+      if (related && model.shouldEagerSaveRelation(fieldName, relation, related)) return true
 
       // All other relations are skipped
-      return false;
+      return false
     }
 
     // Everything else is ok
-    return true;
+    return true
   }
 
   /**
@@ -241,28 +208,23 @@ export default class Transformer {
    * @param {Map<string, Array<string>>} records Map of IDs.
    * @param {ORMModel} record The record to register.
    */
-  private static addRecordForRecursionDetection(
-    records: Map<string, Array<string>>,
-    record: ORMModel
-  ): void {
-    const context: Context = Context.getInstance();
+  private static addRecordForRecursionDetection(records: Map<string, Array<string>>, record: ORMModel): void {
+    const context: Context = Context.getInstance()
 
     if (!record) {
-      context.logger.warn("Trying to add invalid record", record, "to recursion detection");
-      return;
+      context.logger.warn('Trying to add invalid record', record, 'to recursion detection')
+      return
     }
 
     if (!record.$self) {
-      context.logger.warn(
-        "Seems like you're using non-model classes with plugin graphql. You shouldn't do that."
-      );
-      return;
+      context.logger.warn("Seems like you're using non-model classes with plugin graphql. You shouldn't do that.")
+      return
     }
 
-    const model: Model = context.getModel(record.$self().entity);
-    const ids = records.get(model.singularName) || [];
-    ids.push(record.$id!);
-    records.set(model.singularName, ids);
+    const model: Model = context.getModel(record.$self().entity)
+    const ids = records.get(model.singularName) || []
+    ids.push(record.$id!)
+    records.set(model.singularName, ids)
   }
 
   /**
@@ -272,17 +234,17 @@ export default class Transformer {
    * @return {boolean} true when the record is already included in the records.
    */
   private static isRecursion(records: Map<string, Array<string>>, record: ORMModel): boolean {
-    if (!record) return false;
+    if (!record) return false
 
     if (!record.$self) {
       Context.getInstance().logger.warn(
         "Seems like you're using non-model classes with plugin graphql. You shouldn't do that."
-      );
-      return false;
+      )
+      return false
     }
 
-    const model: Model = Context.getInstance().getModel(record.$self().entity);
-    const ids = records.get(model.singularName) || [];
-    return ids.includes(record.$id!);
+    const model: Model = Context.getInstance().getModel(record.$self().entity)
+    const ids = records.get(model.singularName) || []
+    return ids.includes(record.$id!)
   }
 }
